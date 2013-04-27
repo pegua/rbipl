b0b9d9b453b9f72d0683f93a0d94f26fa516cecb
pegua <pegua1@gmail.com>
2013-04-27 20:31:04 +0200

    Updated tree_rotation

--- src/tree_rotation.hpp
+++ src/tree_rotation.hpp
@@ -13,6 +13,28 @@
 
 
 namespace RBIPL {    
+    
+bool addTreeRecursive(SegmentMap::const_iterator old_tree_root, Tree & new_tree, const std::string& hook_name)
+{
+    //get iterator for root-segment
+    SegmentMap::const_iterator child;
+    //try to add all of root's children
+    for (unsigned int i = 0; i < root->second.children.size(); i++) {
+        child = root->second.children[i];
+        //Try to add the child
+        if (new_tree.addSegment(child->second.segment, hook_name)) {
+            //if child is added, add all the child's children
+            if (!(new_tree.addTreeRecursive(child, new_tree, child->first)))
+                //if it didn't work, return false
+                return false;
+        } else
+            //If the child could not be added, return false
+            return false;
+    }
+    return true;
+}
+    
+    
     /**
      * Rotate a KDL::Tree to obtain an equivalent rapresentation, but with a different root link.
      *
@@ -23,12 +45,17 @@ namespace RBIPL {
      * @param new_root_name the name of the link to use as a new root
      * 
      * @return true if all went well, false otherwise
+     * 
+     * \todo add error checking
      */
     bool tree_rotation(const KDL::Tree & old_tree, KDL::Tree & new_tree, const std::string& new_root_name) {
     {
-        SegmentMap::iterator old_root;
-        SegmentMap::iterator new_root;
+        SegmentMap::iterator dummy_root;
+        SegmentMap::iterator old_root; //old *real* root 
+        SegmentMap::iterator new_root; //new *real* root
         SegmentMap segments;
+        unsigned int i; 
+
         
         old_tree.getSegments(segments);
         new_root = segments.find(new_root_name);
@@ -36,19 +63,32 @@ namespace RBIPL {
         if (new_root == segments.end())
             return false;
         
-        //check that the tree has a root segment with a Joint::None
-        old_tree.getRootSegment(old_root);
-        if( old_root.getJoint().getType() != Joint::None )
+        //check that the tree has a root segment with only one child, connected 
+        //with a Joint::None (a fixed joint) (standard in humanoids URDF)
+        old_tree.getRootSegment(dummy_root);
+        if( dummy_root->second.children.size() != 1 ) 
             return false;
             
-        new_tree = KDL::Tree();
+        old_root = dummy_root->second.children[0];
+        if( old_root->second.segment.getJoint().getType() != Joint::None )
+            return false;
         
+        //The dummy_root is moved 
+        new_tree = KDL::Tree(dummy_root->first);
         
-        //the new root is the same segment, but with Joint::None as the joint
-        new_tree.addSegment(Segment(Joint(Joint::None),
+        //the new root is the specified segment, but with Joint::None as the joint
+        new_tree.addSegment(Segment(new_root_name,
+                                    Joint(Joint::None),
                                     Frame::Identity(),
-                                    new_root.),
-                            new_root_name)
+                                    new_root->second.segment.getInertia()),
+                            new_root_name) 
+                            
+        //Now that the new root is inserted, the successor subchain can be simply added
+        //Not efficient, but to avoid code duplication
+        //addTreeRecursive method is private, a copy is done
+        addTreeRecursive(new_root,new_tree,new_root->first);
+        
+        //Now the other part of the tree has to be added 
         
         
             
